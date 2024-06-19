29558caa163bc097aee8990b58262379b5bacfb8
Joshua Ramon Enslin <joshua@museum-digital.de>
2024-06-20 01:05:39 +0200

    Move ICAL feeds config to config file

--- scripts/updateEvents.php
+++ scripts/updateEvents.php
@@ -8,21 +8,9 @@
 declare(strict_types = 1);
 
 require __DIR__ . '/../themes/museum-digital-theme-zola/scripts/IcalReader.php';
+require_once __DIR__ . '/../config/php-conf.conf.php';
 
-const FEEDS = [
-    [
-        'url' => 'https://verein.museum-digital.de/events/liste/?ical=1',
-        'lang' => 'de',
-        'display_urls' => true,
-    ],
-    [
-        'url' => 'https://files.armuli.eu/remote.php/dav/public-calendars/nCCBTtxJ4fKyMTZ5/?export',
-        'lang' => 'en',
-        'display_urls' => false,
-    ],
-];
-
-$reader = new IcalReader(FEEDS);
+$reader = new IcalReader(ICAL_FEEDS);
 $reader->savePages(__DIR__ . '/../content/about/calendar');
 $reader->saveUpcomingToJson(__DIR__ . '/../cached/upcoming_events.json', 'd.m.Y H:i', "d. m.");
 $reader->saveUpcomingToJson(__DIR__ . '/../cached/upcoming_events_local.json', 'd.m.Y H:i', "d. m.", ["uk", "ru"]);
